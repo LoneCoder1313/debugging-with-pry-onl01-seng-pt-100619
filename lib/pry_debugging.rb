4b43cb6111a2524a28dce1af5c19eef5ab3d6a12
Ricky dildy <rdildy92@gmail.com>
2019-10-14 23:41:10 +0000

    Automatically backed up by Learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -2,7 +2,9 @@ require "pry"
 def plus_two(num)
 num + 2
 	num
-	expect {num + 2}.to eq.[:sum]
-#=>	expecting :sum = 5
-	binding.pry 
+	do 
+	expect {num + 2}.to eq.[sum]
+#=>	expecting sum = 5
+	binding.pry
+	end 
 end 
